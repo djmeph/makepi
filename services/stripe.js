032ded18bbb6f47f612eeb90904a5dff3f6b8b06
Norman Witte III <norm.witte@gmail.com>
2020-01-09 23:40:18 -0500

    move getStripeId() call to private function area

--- services/stripe.js
+++ services/stripe.js
@@ -1,31 +1,52 @@
 const StripeConstructor = require('stripe');
 const models = require('../models');
 
-module.exports = {
+let stripe;
+
+const service = {
     customers: {
         retrieve: async (id) => {
-            const { value: stripeKey } = await models.settings.table.get('stripe-key');
-            const stripe = StripeConstructor(stripeKey);
+            await wait(50);
             const customer = await stripe.customers.retrieve(id);
             return customer;
         },
         create: async (id, email) => {
-            const { value: stripeKey } = await models.settings.table.get('stripe-key');
-            const stripe = StripeConstructor(stripeKey);
+            await wait(50);
             const customer = await stripe.customers.create({ id, email });
             return customer;
         },
         createSource: async (id, source) => {
-            const { value: stripeKey } = await models.settings.table.get('stripe-key');
-            const stripe = StripeConstructor(stripeKey);
+            await wait(50);
             const result = await stripe.customers.createSource(id, { source });
             return result;
         },
         deleteSource: async (customerId, sourceId) => {
-            const { value: stripeKey } = await models.settings.table.get('stripe-key');
-            const stripe = StripeConstructor(stripeKey);
+            await wait(50);
             const result = await stripe.customers.deleteSource(customerId, sourceId);
             return result;
         }
     }
 };
+
+module.exports = service;
+
+// Private Functions
+
+getStripeId()
+    .then(((instance) => {
+        stripe = instance;
+    }))
+    .catch((err) => {
+        throw err;
+    });
+
+async function getStripeId() {
+    const { value } = await models.settings.table.get('stripe-key');
+    return StripeConstructor(value);
+}
+
+async function wait(ms) {
+    while (!stripe) {
+        await new Promise((resolve) => setTimeout(resolve, ms));
+    }
+}

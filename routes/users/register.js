c456c800bd706de53c037e4370b131f5d109ba62
Norman Witte III <norm.witte@gmail.com>
2020-01-18 21:53:44 -0500

    Fix error response on register endpoint

--- routes/users/register.js
+++ routes/users/register.js
@@ -16,16 +16,19 @@ module.exports = {
         body: models.users.schema.register.body
     },
     middleware: [async (req, res, next) => {
+        let user;
         try {
             // Create user in db
-            const user = new UserItem(req.body);
+            user = new UserItem(req.body);
 
             // Set to inactive
             user.set('active', models.users.config.active.FALSE);
 
             // Generate Verification Code
             user.set('verificationCode', uuid());
+        } catch (err) { return req.fail(err); }
 
+        try {
             // PUT to db
             await user.create();
 
@@ -37,6 +40,9 @@ module.exports = {
             // Return token
             req.data = { status: 200, response: { token } };
             next();
-        } catch (err) { req.fail(err); }
+        } catch (err) {
+            req.data = { status: 400 || err.status, response: err.message };
+            next();
+        }
     }]
 };

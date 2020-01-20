33cfa4575cc11e5d1aa0af2cf1fcf9b898b6b49f
Norman Witte III <norm.witte@gmail.com>
2020-01-19 21:02:27 -0500

    Acccess-Control-Allow-Credentials

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
     );
     // Set allowed methods
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
 

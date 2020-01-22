1b73043e6ee7209ca8e73f1213ffe6441ed40a07
Norman Witte III <norm.witte@gmail.com>
2020-01-21 23:02:57 -0500

    admin create user

---
+++ routes/users/admin-post.js
@@ -0,0 +1,22 @@
+const models = require('../../models');
+const config = require('../../config');
+
+module.exports = {
+    method: 'POST',
+    endpoint: '/admin/user',
+    access: [config.access.level.keyMaster],
+    validate: {
+        body: models.users.schema.admin.post.body,
+        response: models.users.schema.admin.post.response
+    },
+    middleware: [async (req, res, next) => {
+        try {
+            const user = new models.users.Item(req.body);
+            await user.create();
+            req.data = { status: 200, response: user.get() };
+            next();
+        } catch (err) {
+            req.fail(err);
+        }
+    }]
+};

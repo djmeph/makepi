17bdc3115202156eb04d6ac8cb81498b8f832961
Norman Witte III <norm.witte@gmail.com>
2020-02-02 22:48:08 -0500

    Return payment scheduler

--- tasks/schedule-automatic-payments.js
+++ tasks/schedule-automatic-payments.js
@@ -13,7 +13,6 @@ module.exports = async () => {
                 paymentDate: moment().toISOString(),
             });
             if (!schedules.length) {
-                return console.log(schedules.length);
                 const { planId, versionNumber } = subscription.get('plan');
                 const plan = await models.plans.table.get({
                     planId,

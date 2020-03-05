0caeb91b5157f3e276a5e812066b5150c2ce8a01
Norman Witte III <norm.witte@gmail.com>
2020-03-04 23:49:30 -0500

    Remove unpaid schedules when cancelling membership

--- routes/users/subscriptions/admin-delete-subscription.js
+++ routes/users/subscriptions/admin-delete-subscription.js
@@ -8,11 +8,18 @@ module.exports = {
     middleware: [async (req, res, next) => {
         try {
             const user = await models.users.table.get(req.params.userId);
+            const schedules = await models.schedules.table.getByUserIdAndStatus({
+                userId: req.user.sub,
+                status: models.schedules.config.statuses.unpaid
+            });
             if (!user) {
                 req.data = { status: 404, response: { message: 'NOT FOUND ' } };
                 return next();
             }
             await user.cancelSubscription();
+            await Promise.all(schedules.map(async (schedule) => {
+                await schedule.delete();
+            }));
             req.data = { status: 200 };
             next();
         } catch (err) { req.fail(err); }

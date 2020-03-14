08935682804dead6487bfbc339653997e72220cb
Norman Witte III <norm.witte@gmail.com>
2020-03-14 01:19:46 -0400

    Auto set status paid for zero balance

--- tasks/process-scheduled-payments.js
+++ tasks/process-scheduled-payments.js
@@ -23,61 +23,69 @@ module.exports = async () => {
 
         // If a subscription isn't found, they've paused or cancelled their membership
         if (subscription) {
+            if (balance > 0) {
+                // Get itemKey of payment method
+                // Payment methods must be homogeonized to be agnostic of schema name
+                const paymentMethodKey = subscription.get('paymentMethodKey');
 
-            // Get itemKey of payment method
-            // Payment methods must be homogeonized to be agnostic of schema name
-            const paymentMethodKey = subscription.get('paymentMethodKey');
+                // Cash payment will be processed manually, leave alone.
+                if (paymentMethodKey === 'cash') return false;
 
-            // Cash payment will be processed manually, leave alone.
-            if (paymentMethodKey === 'cash') return false;
+                // Grab schema long name from itemKey
+                const [schema] = paymentMethodKey.split(config.itemKeyDelimiter);
 
-            // Grab schema long name from itemKey
-            const [schema] = paymentMethodKey.split(config.itemKeyDelimiter);
-
-            // Convert long schema name to function name and get payment method item
-            const paymentMethod = await models[config.paymentMethods[schema]].table.get({
-                userId,
-                itemKey: paymentMethodKey
-            });
-
-            // If payment method was deleted, don't process the payment.
-            if (!paymentMethod) return false;
-
-            // Use homogeonous charge() method which returns amount and metadata
-            const { metadata, amount } = await paymentMethod.charge({
-                amount: balance,
-                description: config.APP_NAME
-            });
-
-            // Instantiate and save payment item in pending status
-            const payment = new models.payments.Item({
-                userId,
-                paymentMethodKey: {
+                // Convert long schema name to function name and get payment method item
+                const paymentMethod = await models[config.paymentMethods[schema]].table.get({
                     userId,
                     itemKey: paymentMethodKey
-                },
-                amount,
-                status: models.payments.config.statuses.pending,
-                metadata: { ...metadata },
-            });
-            await payment.create();
+                });
 
-            // Update scheduled item
-            // Set status to paid
+                // If payment method was deleted, don't process the payment.
+                if (!paymentMethod) return false;
+
+                // Use homogeonous charge() method which returns amount and metadata
+                const { metadata, amount } = await paymentMethod.charge({
+                    amount: balance,
+                    description: config.APP_NAME
+                });
+
+                // Instantiate and save payment item in pending status
+                const payment = new models.payments.Item({
+                    userId,
+                    paymentMethodKey: {
+                        userId,
+                        itemKey: paymentMethodKey
+                    },
+                    amount,
+                    status: models.payments.config.statuses.pending,
+                    metadata: { ...metadata },
+                });
+                await payment.create();
+
+                // Update scheduled item
+                // Set status to paid
+                schedule.set('status', models.schedules.config.statuses.paid);
+
+                // Apply payment to scheduled item
+                const payments = schedule.get('payments');
+                payments.push({
+                    paymentId: payment.get('paymentId'),
+                    amount: balance,
+                });
+                schedule.set('payments', payments);
+
+                // Update balance
+                schedule.set('balance', balance - amount);
+
+                // save scheduled item
+                await schedule.update();
+
+                // mark as processed for counter
+                return true;
+            }
+
+            // If zero balance, just set to paid. This is for full scholarships
             schedule.set('status', models.schedules.config.statuses.paid);
-
-            // Apply payment to scheduled item
-            const payments = schedule.get('payments');
-            payments.push({
-                paymentId: payment.get('paymentId'),
-                amount: balance,
-            });
-            schedule.set('payments', payments);
-
-            // Update balance
-            schedule.set('balance', balance - amount);
-
-            // save scheduled item
             await schedule.update();
 
             // mark as processed for counter

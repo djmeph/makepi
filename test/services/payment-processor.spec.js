de231fcd590b9d249fc4838c60b6fe10d4080052
Norman Witte III <norm.witte@gmail.com>
2020-04-08 01:26:40 -0400

    add tests to npm scripts

--- test/services/payment-processor.spec.js
+++ test/services/payment-processor.spec.js
@@ -1,4 +1,15 @@
 const sinon = require('sinon');
 const { expect, assert } = require('chai');
+const PaymentProcessor = require('../../services/payment-processor');
 
-describe('UnitTests::', () => {});
+describe('UnitTests::', () => {
+    describe('PaymentProcessor::', () => {
+        let paymentProcessor;
+        beforeEach(() => {
+            paymentProcessor = new PaymentProcessor();
+        });
+        it('should return false on each schedule item that fails', (done) => {
+            done();
+        });
+    });
+});

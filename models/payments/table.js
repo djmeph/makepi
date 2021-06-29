56347fa326fb625478311754bccfb7d504c8c019
DJ Meph <norm.witte@gmail.com>
2021-06-28 22:42:56 -0400

    Payments table iterate through shards

--- models/payments/table.js
+++ models/payments/table.js
@@ -4,18 +4,27 @@ const config = require('../../config');
 
 class PaymentsTable extends PromisifiedTable {
     async getAllByUserId(userId) {
-        const result = await super.query({
-            KeyConditionExpression: '#id = :id and begins_with(#key, :key)',
-            ExpressionAttributeNames: {
-                '#id': 'userId',
-                '#key': 'itemKey',
-            },
-            ExpressionAttributeValues: {
-                ':id': userId,
-                ':key': `${config.itemKeyPrefixes.payments}${config.itemKeyDelimiter}`,
-            }
-        });
-        return _.get(result, 'Items', []);
+        let result = [];
+        let response;
+        let exclusiveStartKey;
+        do {
+            response = await super.query({
+                KeyConditionExpression: '#id = :id and begins_with(#key, :key)',
+                ExpressionAttributeNames: {
+                    '#id': 'userId',
+                    '#key': 'itemKey',
+                },
+                ExpressionAttributeValues: {
+                    ':id': userId,
+                    ':key': `${config.itemKeyPrefixes.payments}${config.itemKeyDelimiter}`,
+                },
+                ExclusiveStartKey: exclusiveStartKey,
+            });
+            result = [...result, ..._.get(response, 'Items', [])];
+            exclusiveStartKey = response.LastEvaluatedKey;
+        } while (exclusiveStartKey)
+        result = _.sortBy(result, (n) => n.get('paymentDate'));
+        return result;
     }
 }
 
